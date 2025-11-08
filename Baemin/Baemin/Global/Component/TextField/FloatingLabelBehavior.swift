03aac5b34859f5456e3c2b4b8bba9a4a5b4d1576
dudwntjs <sts07190@naver.com>
2025-11-08 13:34:43 +0900

    [fix] 코드리뷰 반영

--- Baemin/Baemin/Global/Component/TextField/FloatingLabelBehavior.swift
+++ Baemin/Baemin/Global/Component/TextField/FloatingLabelBehavior.swift
@@ -5,13 +5,6 @@
 //  Created by sun on 11/1/25.
 //
 
-//
-//  FloatingLabelBehavior.swift
-//  Baemin
-//
-//  Created by sun on 11/1/25.
-//
-
 import UIKit
 
 final class FloatingLabelBehavior {
@@ -101,3 +94,4 @@ final class FloatingLabelBehavior {
         container?.layer.borderColor = focused ? design.borderColorFocused : design.borderColorNormal
     }
 }
+
